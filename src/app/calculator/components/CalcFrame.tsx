1bd6ef39c7e48f2ed40637de241704caadde4295
Lorenzo Canales <lorenzosama@gmail.com>
2024-09-17 15:42:27 -0700

    We have one button working. Now to make the rest work

--- src/app/calculator/components/CalcFrame.tsx
+++ src/app/calculator/components/CalcFrame.tsx
@@ -1,13 +1,87 @@
-import { Container, Paper } from "@mui/material";
+'use client'
+import { Container, Paper, TextField } from "@mui/material";
 import Grid from '@mui/material/Grid2'
 import NumButton from "./NumButton";
 import MathButton from "./MathButton";
+import { Dispatch, useReducer } from "react";
+
 
 
 export default function CalcFrame() {
+
+    type MathOperation = | "clear" | "addition" | "subtration"
+    type CalcAction =
+        | {
+            "type": 'number',
+            "payload": string
+        }
+        | {
+            "type": "math",
+            "payload": MathOperation
+        };
+
+    type CalcState = {
+        displayValue: number,
+        calculation?: string | null
+        //We could store history or something but let's keep it simple
+    };
+
+    const defaultCalcState: CalcState = {
+        displayValue: 0,
+    };
+
+
+    function reducer(calcState: CalcState, action: CalcAction) {
+        switch (action.type) {
+            case 'number':
+                let newValString;
+                if (calcState.displayValue === 0) {
+                    newValString = action.payload
+                }
+                else {
+                    newValString = calcState.displayValue.toString() + action.payload;
+                }
+
+                return ({
+                    ...calcState,
+                    displayValue: parseFloat(newValString)
+                })
+            case 'math':
+                switch (action.payload) {
+                    case 'clear':
+                        return (defaultCalcState);
+                }
+
+            default:
+                return calcState;
+        }
+        return calcState;
+
+    };
+
+
+    const [state, dispatch] = useReducer(reducer, defaultCalcState)
+
+
+    function appendNumber(val: string) {
+        dispatch({ 'type': "number", 'payload': val });
+    }
+
+
     return (
         <Container>
             <Paper elevation={1} sx={{ width: .70 }}>
+                <TextField
+                    id=""
+                    label=""
+                    value={state.displayValue}
+                    slotProps={{
+                        input: {
+                            readOnly: true
+                        },
+                    }}
+
+                />
                 <Grid container spacing={6} columns={4}>
                     <Grid size={1}>
                         <MathButton display={"AC"} />
@@ -23,44 +97,46 @@ export default function CalcFrame() {
                         <MathButton display={"%"} />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={7} />
+                        <NumButton display='7' action={appendNumber} />
+                    </Grid>
+                    {/*
+                    <Grid size={1}>
+                        <NumButton display='8' />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={8} />
-                    </Grid>
-                    <Grid size={1}>
-                        <NumButton display={9} />
+                        <NumButton display='9' />
                     </Grid>
                     <Grid size={1}>
                         <MathButton display={"*"} />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={4} />
+                        <NumButton display='4' />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={5} />
+                        <NumButton display='5' />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={6} />
+                        <NumButton display='6' />
                     </Grid>
                     <Grid size={1}>
                         <MathButton display={"-"} />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={1} />
+                        <NumButton display='1' />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={2} />
+                        <NumButton display='2' />
                     </Grid>
                     <Grid size={1}>
-                        <NumButton display={3} />
+                        <NumButton display='3' />
                     </Grid>
                     <Grid size={1}>
                         <MathButton display={"+"} />
                     </Grid>
                     <Grid size={2}>
-                        <NumButton display={0} />
+                        <NumButton display='0' />
                     </Grid>
+                    */}
                     <Grid size={1}>
                         <MathButton display={"."} />
                     </Grid>
--- src/app/calculator/components/NumButton.tsx
+++ src/app/calculator/components/NumButton.tsx
@@ -1,15 +1,17 @@
 import { Button } from "@mui/material";
 
 interface numButtonProps {
-    display: number
+    display: string
+    action: (val: string) => void,
 
 }
 
 //TODO? Can I combine the diffrent buttons together into one component easily?
 
 export default function NumButton(props: numButtonProps) {
+
     return (
-        <Button fullWidth variant="contained" >
+        <Button fullWidth variant="contained" onClick={() => { props.action(props.display) }}>
             {props.display}
         </Button >
     );
